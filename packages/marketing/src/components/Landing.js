74d8916fa8f5ca18494a73ad84321d445a652ee5
Dhruv Pahwa <dhruv.pahwa762@gmail.com>
2021-05-26 00:32:14 +0530

    Added text on landing page

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const Landing = () => {
     return (
         <div>
-            <h1>Landing</h1>
+            <h1>Landing Page</h1>
             <Link to='/pricing' href='/pricing'>Pricing</Link>
         </div>
     )
